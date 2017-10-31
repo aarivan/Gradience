3bcfffe30c10fb7f3906fc82fc36c8cb316b8370
Abhilash Arivanan <abhilash.arivanan@gmail.com>
2017-10-31 19:50:22 -0400

    Student Account - Edit Profile minor changes

--- src/main/java/org/csc540/app/StudentAccount.java
+++ src/main/java/org/csc540/app/StudentAccount.java
@@ -44,36 +44,38 @@ public class StudentAccount {
 			LOG.info("Invalid Option");
 		}
 	}
-	
-	
-	 public static void studentHomePage(Student currStudent, Scanner scanner) {
-	        int selection = 1;
 
-	        
+	public static void studentHomePage(Student currStudent, Scanner scanner) {
+		int selection = 1;
 
-	        while (selection != 4) {
-	        	System.out.println("::: Student Home Page :::");
-	    		System.out.println(
-	    				"Enter the options for following actions: \n1. View Profile \n2. Edit Profile\n3. View Courses \n4. Log Out \nEnter your option:");
+		while (selection != 4) {
+			System.out.println("::: Student Home Page :::");
+			System.out.println(
+					"Enter the options for following actions: \n1. View Profile \n2. Edit Profile\n3. View Courses \n4. Log Out \nEnter your option:");
 
-	            selection = scanner.nextInt();
+			selection = scanner.nextInt();
+			int newChoice;
+			switch (selection) {
+			case 1:
+				viewProfile(currStudent);
+				newChoice = scanner.nextInt();
+				while (newChoice != 0) {
+					System.out.println("Invalid choice!! Please enter 0 to go back to previous page!!");
+					newChoice = scanner.nextInt();
+				}
+				break;
+			case 2:
+				editProfile(currStudent, scanner);
+				break;
+			case 3:
+				break;
+			default:
+				break;
+			}// end switch
+		}
+	}
 
-	            switch (selection) {
-	                case 1:
-	                    viewProfile(currStudent,scanner);
-	                    break;
-	                case 2:
-	                    editProfile(currStudent,scanner);
-	                    break;
-	                case 3:
-	                    break;
-	                default:
-	                    break;
-	            }//end switch
-	        }
-	    }
-	
-	public static void viewProfile(Student currStudent,Scanner scanner) {
+	public static void viewProfile(Student currStudent) {
 		System.out.println("### Press 0 to Go Back ###\n");
 		System.out.println("1. First Name:" + currStudent.getF_name());
 		System.out.println("2. Last Name:" + currStudent.getL_name());
@@ -82,23 +84,13 @@ public class StudentAccount {
 		System.out.println("5. Phone Number:" + currStudent.getPhone_number());
 		System.out.println("6. Email:" + currStudent.getEmail());
 		System.out.println("### Enter 0 to go back:");
-		int newChoice = scanner.nextInt();
-		if (newChoice == 0) {
-			studentHomePage(currStudent,scanner);
-		}
-		
 	}
-	public static void editProfile(Student currStudent,Scanner scanner) {
-		System.out.println("### Press 0 to Go Back ###\n");
-		System.out.println("1. First Name:" + currStudent.getF_name());
-		System.out.println("2. Last Name:" + currStudent.getL_name());
-		System.out.println("3. Student ID:" + currStudent.getUserId());
-		System.out.println("4. Address:" + currStudent.getAddress());
-		System.out.println("5. Phone Number:" + currStudent.getPhone_number());
-		System.out.println("6. Email:" + currStudent.getEmail());
+
+	public static void editProfile(Student currStudent, Scanner scanner) {
+		viewProfile(currStudent);
 		System.out.println("### Press the field name index to edit that field ###\nEnter your option:");
 		int editChoice = scanner.nextInt();
-		 if (editChoice == 1) {
+		if (editChoice == 1) {
 			System.out.println("Editting First name");
 			System.out.println("Enter the new First name:");
 			String new_F_name = scanner.next();
@@ -106,45 +98,44 @@ public class StudentAccount {
 			try {
 				StudentProcessor.updateStudentProfile(currStudent);
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if (editChoice == 2) {
+		} else if (editChoice == 2) {
 			System.out.println("Editting Last name");
 			System.out.println("Enter the new Last Name:");
 			String new_L_name = scanner.next();
 			currStudent.setL_name(new_L_name);
-			
-		}else if (editChoice == 3) {
+
+		} else if (editChoice == 3) {
 			System.out.println("Editting StudentID");
 			System.out.println("Enter the new Student ID:");
 			String new_StudentID = scanner.next();
 			currStudent.setUserId(new_StudentID);
-			
-		}else if (editChoice == 4) {
+
+		} else if (editChoice == 4) {
 			System.out.println("Editting Address");
 			System.out.println("Enter the new Address:");
 			String new_Address = scanner.next();
 			currStudent.setAddress(new_Address);
-		}else if (editChoice == 5) {
+		} else if (editChoice == 5) {
 			System.out.println("Editting Phone Number");
 			System.out.println("Enter the new Phone Number:");
 			String new_phone_number = scanner.next();
 			currStudent.setPhone_number(new_phone_number);
-		}else if (editChoice == 6) {
+		} else if (editChoice == 6) {
 			System.out.println("Editting Email");
 			System.out.println("Enter the new Email:");
 			String new_email = scanner.next();
 			currStudent.setEmail(new_email);
-		}else if (editChoice == 0) {
+		} else if (editChoice == 0) {
 			try {
 				StudentProcessor.updateStudentProfile(currStudent);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			studentHomePage(currStudent,scanner);
+			studentHomePage(currStudent, scanner);
 		}
-	
+
 	}
 }
